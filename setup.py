ad39fc0b0d92bd6231b4d6735f38bb40a9ff6942
Ankit Khambhati <akhambhati@gmail.com>
2019-11-12 00:07:52 -0800

    Added packaging data

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zappy',
+    version='0.1.0',
+    author='Ankit N. Khambhati',
+    author_email='akhambhati@gmail.com',
+    packages=find_packages(exclude=('test', 'docs')),
+    include_package_data=True,
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[])
