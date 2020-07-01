da920f8d0207fef4c4ed70764affc11d1d664186
Ankit Khambhati <akhambhati@gmail.com>
2020-07-01 16:46:13 -0700

    Added pyeisen and pyfftw to the requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages, setup
 
+requirements = [*map(str.strip, open("requirements.txt").readlines())]
+
 setup(
     name='zappy',
     version='0.1.0',
@@ -9,4 +11,7 @@ setup(
     include_package_data=True,
     license='MIT',
     long_description=open('README.md').read(),
-    install_requires=[])
+    install_requires=requirements,
+    dependency_links=[
+        'git+http://github.com/akhambhati/pyEisen.git@8ffc18cf3d1413c960336b48abcd3771c42c014d#egg=pyEisen-1.0.0']
+)
